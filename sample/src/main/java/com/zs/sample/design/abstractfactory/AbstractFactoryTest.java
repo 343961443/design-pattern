0ae3fe7c9c6b1118c00453bf5341cc961c88e92a
zhaoshuai <zhaoshuai@huya.com>
2021-04-20 09:25:42 +0800

    提交下代码

--- sample/src/main/java/com/zs/sample/design/abstractfactory/AbstractFactoryTest.java
+++ sample/src/main/java/com/zs/sample/design/abstractfactory/AbstractFactoryTest.java
@@ -6,17 +6,22 @@ package com.zs.sample.design.abstractfactory;
  */
 public class AbstractFactoryTest {
     public static void main(String[] args) {
+        // 获得形状工厂
         AbstractDrawFactory shapeFactory = FactoryProducer.getFactory("shape");
+        // 画圆
         Shape circleShape = shapeFactory.getShape("circle");
         circleShape.draw();
 
+        // 画方
         Shape squareShape = shapeFactory.getShape("square");
         squareShape.draw();
 
+        // 获得颜色工厂
         AbstractDrawFactory colorFactory = FactoryProducer.getFactory("color");
+        // 填充红色
         Color redColor = colorFactory.getColor("red");
         redColor.fill();
-
+        // 填充绿色
         Color greenColor = colorFactory.getColor("green");
         greenColor.fill();
     }
