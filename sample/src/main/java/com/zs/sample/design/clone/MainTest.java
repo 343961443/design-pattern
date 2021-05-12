ac2f8876d0f744d133789f9c320783ac8bb4c800
zhaoshuai <zhaoshuai@huya.com>
2021-05-12 16:30:57 +0800

    注释搞一搞

--- sample/src/main/java/com/zs/sample/design/clone/MainTest.java
+++ sample/src/main/java/com/zs/sample/design/clone/MainTest.java
@@ -15,20 +15,20 @@ public class MainTest {
         System.out.println("book1=>" + book1);
         // object 进行克隆
         Book book2 = (Book)book1.clone();
-        System.out.println("book2=>" + book2);
-        System.out.println("book1 == book2 =>" + (book1 == book2));
+        System.out.println("浅克隆book2=>" + book2);
+        System.out.println("浅克隆book1 == book2 =>" + (book1 == book2));
         // 浅克隆模式，那么克隆出来的对象的属性subBook就是同一个
-        System.out.println("book2.getSubBook == book1.getSubBook=>" + (book2.getBook() == book1.getBook()));
+        System.out.println("浅克隆book2.getSubBook == book1.getSubBook=>" + (book2.getBook() == book1.getBook()));
 
         // 流化克隆
         Book book3 =  (Book)book1.streamClone();
-        System.out.println("book3=>" + book3);
-        System.out.println("book1 == book3 =>" + (book1 == book3));
-
+        System.out.println("流化克隆book3=>" + book3);
+        System.out.println("流化克隆book1 == book3 =>" + (book1 == book3));
+        System.out.println("流化克隆book3.getSubBook == book1.getSubBook=>" + (book3.getBook() == book1.getBook()));
 
         // 深克隆模式，那么克隆出来的对象的属性subBook不是同一个
         Book book4 =  (Book)book1.deepClone();
-        System.out.println("book4.getSubBook == book1.getSubBook=>" + (book4.getBook() == book1.getBook()));
+        System.out.println("深克隆book4.getSubBook == book1.getSubBook=>" + (book4.getBook() == book1.getBook()));
 
 
 
