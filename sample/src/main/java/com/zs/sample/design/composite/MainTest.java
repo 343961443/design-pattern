7857bc9263b2e11653777f4cb1dd9cf274b9cd20
zhaoshuai <zhaoshuai@huya.com>
2021-05-21 09:39:52 +0800

    组合模式

---
+++ sample/src/main/java/com/zs/sample/design/composite/MainTest.java
@@ -0,0 +1,21 @@
+package com.zs.sample.design.composite;
+
+/**
+ * @Author: zhaoshuai
+ * @Date: 2021/5/21
+ */
+public class MainTest {
+    public static void main(String[] args) {
+        Employee CEO = new Employee("John","CEO", 30000);
+
+        Employee headSales = new Employee("Robert","Head Sales", 20000);
+
+        Employee headMarketing = new Employee("Michel","Head Marketing", 20000);
+
+        CEO.add(headSales);
+
+        CEO.add(headMarketing);
+
+        System.out.println(CEO);
+    }
+}
